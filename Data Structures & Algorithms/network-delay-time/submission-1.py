2bd11680cc0fda5a2311932a7f5f8052f481d5ce
peeyush <peeyush113@gmail.com>
2026-06-12 23:30:48 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = {i: [] for i in range(1, n+1)}
+        for u, v, t in times:
+            graph[u].append([t, v])
+
+        heap = [[0, k]]
+        res = {}
+        while heap:
+            t, v = heapq.heappop(heap)
+            if v in res:
+                continue
+            res[v] = t
+            for tv, vv in graph[v]:
+                if vv not in res:
+                    heapq.heappush(heap, [t+tv, vv])
+        return max(list(res.values())) if len(res) == n else -1
