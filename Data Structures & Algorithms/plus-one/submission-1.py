e383eeed1bcb227ddeec737dfb65ff53a3665c12
peeyush <peeyush113@gmail.com>
2026-06-22 23:50:18 +0530

    Add: plus-one - submission-1

---
+++ Data Structures & Algorithms/plus-one/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        resp = []
+        carry = 1
+        for r in range(len(digits)-1, -1, -1):
+            n = digits[r] + carry
+            carry = n//10
+            resp.append(n%10)
+            print(r,n, carry, resp)
+        if carry:
+            resp.append(carry)
+        print(resp)
+        op = []
+        for i in range(len(resp)-1, -1, -1):
+            op.append(resp[i])
+        return op
