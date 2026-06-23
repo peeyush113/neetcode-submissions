52b845a254edbe6a5a6d75348643a597c7af94b0
peeyush <peeyush113@gmail.com>
2026-06-23 08:42:22 +0530

    Add: add-binary - submission-1

---
+++ Data Structures & Algorithms/add-binary/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        resp = ""
+        l, r = len(a)-1, len(b)-1
+
+        carry = 0
+        while l>-1 or r>-1:
+            if l>-1:
+                carry += int(a[l])
+                l -= 1
+            if r > -1:
+                carry += int(b[r])
+                r -= 1
+            resp = str(carry%2) + resp
+            carry = carry//2
+        
+        if carry:
+            resp = str(carry) + resp
+        return resp
