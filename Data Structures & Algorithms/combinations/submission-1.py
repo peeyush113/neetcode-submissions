ff6bf9309c27e229d52f3c6d8142115816f7b4c8
peeyush <peeyush113@gmail.com>
2026-06-24 17:36:27 +0530

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        op = []
+
+        def backtrack(i, resp):
+            if len(resp) == k:
+                op.append(resp.copy())
+                return
+            if i>n:
+                return 
+            for j in range(i, n+1):
+                resp.append(j)
+                backtrack(j+1, resp)
+                resp.pop()
+        
+        backtrack(1, [])
+        return op
